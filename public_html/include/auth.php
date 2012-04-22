bcb6811423f099b6053c232c1badbad0bf70f80b
dasasha <fedorow.alexander@gmail.com>
2012-04-22 15:23:49 +0400

    Добавлены инклуды в php.Просто так эта версия сайта уже работать не будет, ее нужно запускать сервера. Обновлена главная страница. Пока что не работает min-height (из-за этого все записи имееют фиксированную высоту). Не тестировалась большая длина текста в записях.

---
+++ public_html/include/auth.php
@@ -0,0 +1,11 @@
+﻿<div id="authorisation">
+			<form method="post" action="login.php">
+				<input id="email" name="email" type="text" placeholder="Email">
+				<input id="password" name="password" type="password" placeholder="Пароль"><br>
+				<input id="cookie" name="cookie" type="checkbox"><span id="checkbox_text">запомнить<span>
+				<input id="primary-btn" type="submit" value="Войти">
+			</form>
+			<form action="reg.php">
+				<input id="signup-btn" type="submit" value="Регистрация">
+			</form>
+		</div>

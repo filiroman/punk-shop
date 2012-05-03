fe1a0303afe80032bd1f1fe5e35227ebf2e517a8
dasasha <fedorow.alexander@gmail.com>
2012-05-03 19:41:05 +0400

    fix for "Vi zashli kak Vasiliy"

--- public_html/include/user_bar.php
+++ public_html/include/user_bar.php
@@ -1,5 +1,16 @@
 ﻿<div id="user_bar">
-			<span style="margin: 10px;">Вы зашли как Василий</span>
+			<?php
+				$hostname = "localhost";
+				$username = "root";
+				$password = "";
+				$dbName = "punk_shop";
+				mysql_connect($hostname, $username) or die("Не могу создать соединение");
+				mysql_select_db($dbName) or die(mysql_error()); 
+				$query = mysql_query("SELECT name FROM users WHERE id='".$_SESSION['id']."' LIMIT 1");
+				$data = mysql_fetch_assoc($query);
+				mysql_close();
+			?>
+			<span style="margin: 10px;">Вы зашли как <?php echo $data['name']; ?></span> <!-- временный вывод имени !-->
 			<button class="btn" ><a href="user_logout.php">Выйти</a></button></br>
 			<button class="btn" ><a href="user_info.php">Личный кабинет</a></button>
 			<button class="btn" ><a href="item_add.php">Добавить товар</a></button>
---
+++ public_html/login.php
@@ -0,0 +1,22 @@
+ <?php
+	session_start();
+	$hostname = "localhost";
+	$username = "root";
+	$password = "";
+	$dbName = "punk_shop";
+	mysql_connect($hostname, $username) or die("Не могу создать соединение");
+	mysql_select_db($dbName) or die(mysql_error()); 
+	
+	$query = mysql_query("SELECT id, pass FROM users WHERE email='".mysql_real_escape_string($_POST['email'])."' LIMIT 1");
+	$data = mysql_fetch_assoc($query);
+	mysql_close();
+	if($data['pass'] === md5(md5($_POST['password']))) {
+		$_SESSION['id'] = $data['id'];
+		header("Location: index.php");
+	} else{
+		echo "<p style=\"font-family: helvetica; font-size: 20pt;\">Вы ввели неправильный логин/пароль</p>";
+	}
+	
+	
+?>
+ 

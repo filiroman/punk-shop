9e6e4e8f6b9e769731b5602dd40e8752b409e08a
Evgeniy Bel <chromigo@gmail.com>
2012-05-08 01:25:07 +0400

    yii-punk-shop starter kit

---
+++ punk-shop_yii/punk-shop(OLD)/public_html/item_info.php
@@ -0,0 +1,38 @@
+﻿<!DOCTYPE HTML>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/reset.css"/>
+	<link rel="stylesheet" type="text/css" href="css/index.css"/>
+	<link rel="stylesheet" type="text/css" href="css/item_info.css"/>
+</head>
+
+<body>
+	<?php 
+	$auth=false;
+	include "include/index_head.php" ?>
+
+	<div id="main">
+	
+		<?php include "include/search_categories.php" ?>
+		
+		<div id="item">
+			
+			<p id="item_name"><a href="item_info.php">Холодильник</a></p> 			
+			<span style="float:left;margin:0 5px 0 10px;">разместил</span><p id="item_author"><a href="user_info.php">Василий</a></p><p id="item_date">14.03.2012</p></br>
+			<img id="item_img" src="img/pic3.jpg">
+			<img id="item_img_mini" src="img/pic3.jpg">
+			<img id="item_img_mini" src="img/pic3_2.jpg">
+			<img id="item_img_mini" src="img/pic3_3.jpg">
+			<img id="item_img_mini" src="img/pic3_4.jpg">
+			<p id="item_description">Холодильник Бирюза в отличном состоянии. Рабочая морозилка. Самовывоз. 15ка</p>
+			<p id="item_price">цена: <span style="font-size:15px;font-weight:bold">2100</span>р</p>
+			<p id="item_contact">8-911-1234567</p>
+			<p id="item_contact">email@mail.ru</p>
+			
+		</div>
+
+	</div>
+	
+	<?php include "include/index_foot.php" ?>
+</body>
+</html>
---
+++ punk-shop_yii/punk-shop(OLD)/public_html/user_add.php
@@ -0,0 +1,30 @@
+﻿<!DOCTYPE HTML>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/reset.css"/>
+	<link rel="stylesheet" type="text/css" href="css/index.css"/>
+	<link rel="stylesheet" type="text/css" href="css/user_add.css"/>
+</head>
+
+<body>
+	<?php 
+	$auth=false;
+	include "include/index_head.php" ?>
+
+	<div id="main">
+		<p>Регистрация</p>
+		<form method="post" action="user_add_action.php">
+			<span>email</span><input id="user_email" name="email" type="text"><br>
+			<span>имя</span><input id="user_name" name="name" type="text"><br>
+			<span>пароль</span><input id="user_password" name="password" type="password"><br>
+			<span>пароль</span><input id="user_password" name="password" type="password"><br>
+			<input id="reg_button" type="submit" value="Зарегистрировать"><br>
+		</form>
+		
+	</div>
+
+	<?php include "include/index_foot.php" ?>
+</body>
+
+
+</html>

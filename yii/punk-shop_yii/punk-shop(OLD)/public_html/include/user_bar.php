04f62d5324bc4afd9a1575dd3eb0d9011742fb0f
Filippov Roman <filiroman.tsu@gmail.com>
2012-05-15 20:39:01 +0400

    some deletions

--- yii/punk-shop_yii/punk-shop(OLD)/public_html/include/user_bar.php
+++
@@ -1,6 +0,0 @@
-﻿<div id="user_bar">
-			<span style="margin: 10px;">Вы зашли как Василий</span>
-			<button class="btn" >Выйти</button></br>
-			<button class="btn" ><a href="user_info.php">Личный кабинет</a></button>
-			<button class="btn" ><a href="item_add.php">Добавить товар</a></button>
-		</div>

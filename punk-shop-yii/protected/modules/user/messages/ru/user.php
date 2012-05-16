4d824452ac6ecc20ecd72e63269194bda2068cb7
Slackerburst <slackerburst@slackerburst-VB-PC.(none)>
2012-05-16 20:36:04 +0400

    Added lines in autochange file.

--- punk-shop-yii/protected/modules/user/messages/ru/user.php
+++ punk-shop-yii/protected/modules/user/messages/ru/user.php
@@ -124,5 +124,6 @@ return array(
 	'The value of the default field (database).' => 'Значение поля по умолчанию (база данных).',
 	'Display order of fields.' => 'Порядок отображения полей.',
 	'Not visited' => 'Не входил',
+	'Birthday' => 'Дата рождения',
 );
 

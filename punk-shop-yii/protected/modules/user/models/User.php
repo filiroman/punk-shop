ec41eea69a095820d18011b9da1acf8085f87549
Andreika111 <poleno.spb@gmail.com>
2012-05-17 03:31:01 +0400

    проверили что номер состоит из цифр, и его длинна не превышает 11(в бд размер телефона = 11)

--- punk-shop-yii/protected/modules/user/models/User.php
+++ punk-shop-yii/protected/modules/user/models/User.php
@@ -48,6 +48,9 @@ class User extends CActiveRecord
 			#array('username, password, email', 'required'),
 			array('username', 'length', 'max'=>20, 'min' => 3,'message' => UserModule::t("Incorrect username (length between 3 and 20 characters).")),
 			array('password', 'length', 'max'=>128, 'min' => 4,'message' => UserModule::t("Incorrect password (minimal length 4 symbols).")),
+			#	проверяем телефонный номер
+			array('phone','lengt','max'=>11,'message'=>UserModule::t("Incorrect phone number.")),
+			array('phone', 'match', 'pattern' => '/^[0-9]+$/u','message' => UserModule::t("Incorrect phone number: use numbers only.")),
 			array('email', 'email'),
 			array('username', 'unique', 'message' => UserModule::t("This user's name already exists.")),
 			array('email', 'unique', 'message' => UserModule::t("This user's email address already exists.")),

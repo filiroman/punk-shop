b53775d52d5bf6be3a140b49f29bdfe48066e281
Filippov Roman <filiroman.tsu@gmail.com>
2012-05-18 01:14:35 +0400

    main page fixes

---
+++ punk-shop-yii/protected/views/site/index2.php
@@ -0,0 +1,23 @@
+<?php $this->pageTitle=Yii::app()->name; ?>
+
+<div id="categories">
+	<ul>
+	<?php foreach(Categories::getIdsWithTitles() as $category) { ?>
+            <li>
+                <a href="index.php?category_id=<?php echo $category['id']; ?>"><?php echo $category['title']; ?></a>
+            </li>            
+        <?php } ?>
+	</ul>
+</div>
+
+
+
+<div id="result">	
+
+    <?php $this->widget('zii.widgets.CListView', array(
+            'dataProvider'=> $dataProvider,
+            'itemView'=>'../goods/_view',
+    )); ?>
+	
+</div>
+
